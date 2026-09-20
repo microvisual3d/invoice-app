8dc7ea3e7b5b29e6439a930664c15f4a4df98755
microvisual3d <derek.ho@doctors.org.uk>
2026-09-20 21:14:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='invoices-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(ch=>ch.put(e.request,c));return r;}).catch(()=>caches.match(e.request).then(m=>m||caches.match('./index.html'))));
+});
